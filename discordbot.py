83240580d119f756d75b090a42343f4632ef4719
Manju-Kei1023 <52351018+Manju-Kei1023@users.noreply.github.com>
2020-01-03 19:45:20 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,25 @@
 from discord.ext import commands
 import os
 import traceback
+import discord
+
+client = discord.Client()
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+    if message.content == '!?test':
+        await message.channel.send('あー')
 
 bot.run(token)
