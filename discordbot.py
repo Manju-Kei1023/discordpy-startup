4ca078335495a083ae9e37e4cb43da00972e0f89
Manju-Kei1023 <52351018+Manju-Kei1023@users.noreply.github.com>
2020-01-05 01:08:05 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -9,10 +9,17 @@ async def on_ready():
     print('bonjin neko bot is starting')
     print('Logging as Name:'+str(client.user.name)+'\nID:'+str(client.user.id))
     prefix = 'b!'
+    args = []
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
+    args = message.content[len(prefix):].split(' ')
+    if message.content.startsWith(prefix):
+        if args[0] == 'help':
+            await message.channel.send('開発中')
+        
+        
 
 client.run(token)
