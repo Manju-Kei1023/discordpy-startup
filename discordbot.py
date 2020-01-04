45677c7df3194e3d0723a12fd22fbea91a39ee47
Manju-Kei1023 <52351018+Manju-Kei1023@users.noreply.github.com>
2020-01-04 22:56:26 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,7 +11,7 @@ async def on_ready():
     print('bonjin neko bot is start!')
     print('Logind as\n'+str(bot.user.name)+'\n'+str(bot.user.id))
 
-@bot.event
+@bot.listen()
 async def on_message(message):
     if message.author.bot:
         return
