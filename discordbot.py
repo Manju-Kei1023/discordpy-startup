082c63461af7f566a0b5d3758ecb195a5d9ef12d
Manju-Kei1023 <52351018+Manju-Kei1023@users.noreply.github.com>
2020-01-05 01:32:14 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import os
 
-bot = commands.Bot(command_prefix='b!' ,help_command=None)
+bot = commands.Bot(command_prefix='b!' ,help_command=None ,Activity='凡人ねこBot')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 @bot.event
