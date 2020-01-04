82c86bd0f6f1ac76596ca898e0fcf464a1a46a2b
Manju-Kei1023 <52351018+Manju-Kei1023@users.noreply.github.com>
2020-01-04 22:11:31 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,6 +12,32 @@ token = os.environ['DISCORD_BOT_TOKEN']
 async def on_ready():
     print('bonjin neko bot is start!')
     print('Logind as\n'+str(bot.user.name)+'\n'+str(bot.user.id))
+    client.global_list = []
+
+@client.event
+async def on_message(message):
+
+    if message.author == message.guild.me:
+        return
+
+    if message.webhook_id:
+        return
+
+    global_tmp = [w for w in await message.channel.webhooks() if w in client.global_list]
+
+    if message.content == "!global":
+        if global_tmp:
+            await message.channel.send("既に登録されています。")
+            return
+
+        new_w = await message.channel.create_webhook(name="global")
+        client.global_list.append(new_w)
+        await message.channel.send("グローバルチャットのチャンネルに登録しました。")
+        return
+
+    for webhook in client.global_list:
+        if message.channel != webhook.channel:
+            await webhook.send(content=message.content,username=message.author.name,avatar_url=message.author.avatar_url)
 
 @client.event
 async def on_message(message):
