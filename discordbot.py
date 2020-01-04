ebcbc25e40c616275db729c930cff5afd4a48367
Manju-Kei1023 <52351018+Manju-Kei1023@users.noreply.github.com>
2020-01-04 18:30:16 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,7 +1,9 @@
 import discord
 from discord.ext import commands
 import os
-import traceback
+import tracebac
+
+#client = discord.Client()
 
 bot = commands.Bot(command_prefix='b!')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -13,7 +15,7 @@ async def on_ready():
 
 @bot.event
 async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
+    orig_error = getattr(error, "問題が発生しました", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
