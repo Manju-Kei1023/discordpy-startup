18ce046653d65411a5e885bd649855d3d4ae17e3
Manju-Kei1023 <52351018+Manju-Kei1023@users.noreply.github.com>
2020-01-04 23:05:36 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,36 +1,9 @@
 import discord
-from discord.ext import commands
 import os
-import traceback
 
-bot = commands.Bot(command_prefix='b!')
+client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
 
-@bot.event
-async def on_ready():
-    print('bonjin neko bot is start!')
-    print('Logind as\n'+str(bot.user.name)+'\n'+str(bot.user.id))
-
-@bot.listen()
-async def on_message(message):
-    if message.author.bot:
-        return
-    if '草' in message.content:
-        await message.channel.send('草')
 
 
-
-@bot.command()
-async def say(ctx, *args):
-    await ctx.send('{}'.format(' '.join(args)))
-
-@bot.command()
-async def cat(ctx):
-    await ctx.send('にゃーん')
-    await ctx.send("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
-
-@bot.command()
-async def mydata(ctx):
-    await ctx.send('Your ID Here:'+str(ctx.author.id))
-
-bot.run(token)
+client.run(token)
