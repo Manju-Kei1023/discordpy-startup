d5ce013db498f506e39f084947ad50f73a751eb1
Manju-Kei1023 <52351018+Manju-Kei1023@users.noreply.github.com>
2020-01-04 23:15:42 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,6 +4,15 @@ import os
 client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
 
+@client.event
+async def on_ready():
+    print('bonjin neko bot is starting')
+    print('Logging as Name:'+client.user.name+'\nID:'+client.user.id)
+    prefix = 'b!'
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
 client.run(token)
